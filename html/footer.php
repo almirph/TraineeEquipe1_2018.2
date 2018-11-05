681a70cf370e55a14e47d6b0061ebc0d44c70e7e
josueodh <josuedelgadoheringer98@gmail.com>
2018-11-05 18:41:16 -0200

    Mudando arquivo para php e separado header e footer

---
+++ html/footer.php
@@ -0,0 +1,228 @@
+
+		<div class="rodape">
+
+<footer>
+
+    <div class="container-fluid text-center">
+
+            <div class="row informaçoes-primarias py-3">
+            
+                <div class="col-lg-3 pdd-special">
+
+                    
+                    <h5 id="fale-conosco">Fale conosco:<span style="font-size: 26px"><button class="fas fa-headset loc-tamanho" type="button" class="btn btn-primary " data-toggle="modal" data-target="#exampleModal"></button></span></h5> 
+
+                <div class="modal fade faleconosco-footer" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h3 class="modal-title" id="exampleModalLabel">Fale conosco</h3>
+                        
+                        
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      <div class="modal-body faleconosco">
+
+                        <form>
+                          
+                  <div class="form-row">
+
+                    <div class="form-group col-sm-6">
+
+                      <label for="nome" style="text-align: left;">Nome:</label>
+                      <input type="text" class="form-control areatexto" id="name" placeholder="Digite seu nome">
+
+                    </div>
+
+                     <div class="form-group col-sm-6">
+
+                      <label for="sobrenome">Sobrenome:</label>
+                      <input type="text" class="form-control areatexto" id="name" placeholder="Digite seu sobrenome">
+                      
+                    </div>
+
+                 </div>
+
+                 <div class="form-row">
+
+                    <div class="form-group col-sm-12">
+
+                        <label for="email">E-mail:</label>
+                          <input type="email" class="form-control areatexto" id="email" placeholder="Digite seu e-mail">
+                     </div>
+
+
+                 </div>
+
+                    <div class="form-row">
+
+                  <div class="form-group col-sm-12">
+
+                        <label for="email">Mensagem:</label>
+                          <textarea class="form-control areatexto" rows="8" placeholder="Digite sua mensagem"></textarea>
+                  </div>
+
+
+                </div>
+
+                        </form>
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-outline-dark">Enviar</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+            
+            
+            
+
+                        <ul class="list-unstyled">
+
+                            <li>(31)3456-0967</li>
+                            <li>(31)3478-2331</li>
+                        </ul>
+                    
+                </div>
+
+                <div class="col-lg-3">
+                    
+                    <h5>Horário de atendimento:</h5>
+
+                        <ul class="list-unstyled">
+                            <li>8h00 às 18h00</li>
+                            <li>Segunda à Sábado</li>
+                        </ul>           
+                </div>
+
+                <div class="col-lg-3 pdd-special ">
+
+                    <h5>Nossa localização:<span style="font-size: 26px"><button class="fas fa-map-marker-alt loc-tamanho" type="button" class="btn btn-primary " data-toggle="modal" data-target="#exampleModalCenter"></button></span></h5>  
+
+            <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">	
+                        <div class="modal-body">
+                            <iframe  class="col-lg-12" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3655.7803056300645!2d-46.668182784405964!3d-23.612210769358498!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5a0bf7346baf%3A0xd756d1a0261c4c1a!2sBal%C3%A3o+da+Inform%C3%A1tica+S%C3%A3o+Paulo+Moema!5e0!3m2!1spt-BR!2sbr!4v1539034124567" width="650" height="450" frameborder="0" style="border:1" allowfullscreen></iframe>
+                        </div>
+                </div>	  
+            </div>
+
+
+                    <ul class="list-unstyled">
+
+                        <li>Av. Presidente Costa e Silva</li>
+                        <li>Juiz de Fora - MG</li>
+                    </ul>
+
+
+                    
+                </div>
+
+                <div class="col-lg-3">
+                    
+                    <h5>Receba nossas novidades por <br> e-mail:</h5>
+
+                    <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="E-mail" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                            <div class="input-group-append">
+                                <button class="btn btn-outline-secondary" type="button" style="color: white">Cadastrar</button>
+                            </div>
+                        </div>  
+                                    
+                    </div>
+
+                </div>
+
+         </div>
+
+            <div class="container-fluid text-center">
+
+
+                <div class = "row text-center icons">
+
+                    <div class="col-lg-12 text-center py-2">
+                
+                        <span style="font-size: 32px"><a href="https://www.facebook.com/balaodainformatica.com.br/"><i class="fab fa-facebook-f"></i></a></span>
+                        <span style="font-size: 32px"><a href="https://twitter.com/balaoinfo"><i class="fab fa-twitter"></i></a></span>
+                        <span style="font-size: 32px"><a href="https://www.instagram.com/balaoitatiba/"><i class="fab fa-instagram"></i></a></span>
+                        <span style="font-size: 32px"><a href="https://plus.google.com/discover"><i class="fab fa-google-plus-g"></i></a></span>
+                        <span style="font-size: 12px" class="lock" data-toggle="modal" data-target="#login"><i class="fas fa-lock"></i></span>
+
+                        
+
+                            <div class="modal fade faleconosco-footer" id="login" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                              <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                  <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Tela do usuário</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                      <span aria-hidden="true">&times;</span>
+                                    </button>
+                                  </div>
+                                  <div class="modal-body">
+                                    <form>
+                                          <div class="form-group">
+                                            <label for="exampleInputEmail1">E-mail</label>
+                                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Digite seu e-mail">
+                                            
+                                          </div>
+                                          <div class="form-group">
+                                            <label for="exampleInputPassword1">Senha</label>
+                                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Digite sua senha">
+                                          </div>
+                                          <hr>
+                                              <button type="submit" class="btn btn-outline-dark">Login</button>
+                                         
+                                        </form>
+                                  </div>
+                                 
+                                </div>
+                              </div>
+                            </div>
+                        
+                        
+                    </div>
+
+                </div>
+
+            
+            </div>
+
+</footer>
+
+</div>
+
+
+<!-- Fim do footer -->
+<!------------------------------------------------------------------------------------------------------------------>
+<!------------------------------------------------------------------------------------------------------------------>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+crossorigin="anonymous"></script>
+
+
+
+
+
+
+
+</html>
---
+++ html/paginaProdutos.php
@@ -0,0 +1,92 @@
+
+
+<?php include 'Header.php'; ?>
+
+    <!------------------------------------------------- FIM NAVBAR ----------------------------------------------------->
+    
+
+
+    <nav class="navbar navbar-expand-lg navbar-light categoria">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01"
+        aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+
+        <ul class="list-group mr-auto mt-2 mt-lg-0 ">
+          <a href="#">
+            <li class="list-group-item d-flex justify-content-between align-items-center lista-item">
+              Categoria 1
+              <span class="badge badge-primary badge-pill nprodutos"></span>
+            </li>
+          </a>
+          <a href="#">
+            <li class="list-group-item d-flex justify-content-between align-items-center lista-item">
+              Categoria 2
+              <span class="badge badge-primary badge-pill nprodutos"></span>
+            </li>
+          </a>
+          <a href="#">
+            <li class="list-group-item d-flex justify-content-between align-items-center lista-item">
+              Categoria 3
+              <span class="badge badge-primary badge-pill nprodutos"></span>
+            </li>
+          </a>
+          <a href="#">
+            <li class="list-group-item d-flex justify-content-between align-items-center lista-item">
+              Categoria 4
+              <span class="badge badge-primary badge-pill nprodutos"></span>
+            </li>
+          </a>
+          <a href="#">
+            <li class="list-group-item d-flex justify-content-between align-items-center lista-item">
+              Categoria 5
+              <span class="badge badge-primary badge-pill nprodutos"></span>
+            </li>
+          </a>
+          <a href="#">
+            <li class="list-group-item d-flex justify-content-between align-items-center lista-item">
+              Categoria 6
+              <span class="badge badge-primary badge-pill nprodutos"></span>
+            </li>
+          </a>
+          <a href="#">
+            <li class="list-group-item d-flex justify-content-between align-items-center lista-item">
+              Categoria 7
+              <span class="badge badge-primary badge-pill nprodutos"></span>
+            </li>
+          </a>
+          <a href="#">
+            <li class="list-group-item d-flex justify-content-between align-items-center lista-item">
+              Categoria 8
+              <span class="badge badge-primary badge-pill nprodutos"></span>
+            </li>
+          </a>
+          <a href="#">
+            <li class="list-group-item d-flex justify-content-between align-items-center lista-item">
+              Categoria 9
+              <span class="badge badge-primary badge-pill nprodutos"></span>
+            </li>
+          </a>
+
+        </ul>
+
+      </div>
+    </nav>
+    </nav>
+    <div class="container titulo-pag-produtos">
+
+      <h1>Nome do produto</h1>
+      <div class="img-produto-pagina">
+        <img src="../img/teclado-pagprodutos.jpg" class="img-fluid img-pagina" alt="Imagem responsiva">
+      </div>
+      <div class=texto>
+      <h2>Informações do Produto</h2>
+      <p>Teclado com teclas "chiclet" totalmente programáveis com função de gravação de macros. O formato slim das teclas com 2 milímetros do Razer Deathstalker garantem menos tempo para o acionamento das mesmas, com uma reação mais rápida a diversas situações e inimigos. Seus dedos podem se mover rapidamente entre as teclas, assegurando que o seu plano para dominar o jogo inteiro seja executado imediatamente.
+        Tecnologia anti-ghosting para o pressionamento simultâneo de até 10 teclas. Habilitado para o Razer Synapse 2.0, sincronize o cérebro do Razer DeathStalker Expert com um servidor de nuvem para baixar o driver e atualizações de firmware, bem como salvar as suas configurações individuais.</p>
+
+    </div>
+    </div>
+
+<!------------------------------------------- Início FOOTER ----------------------------------------------------------->
+       <?php include 'footer.php'?>
