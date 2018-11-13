3395032224ace646c502bc8367741eed2459210d
Pedro Alves <barbosa1@ice.ufjf.br>
2018-11-13 14:15:42 -0200

    Arrumando css

--- html/Quemsomos.php
+++ html/Quemsomos.php
@@ -1,183 +1,81 @@
 
 <?php include 'Header.php'; ?>	
-	
 
-	<div class="body-quemsomos">
-		<div class="container">
-			<div class="Titulo-quemsomos">
-				<h1 class="pb-2 mt-4 mb-2 border-bottom">Quem somos</h1>
-			</div>
+
+<div class="body-quemsomos">
+	<div class="container">
+		<div class="Titulo-quemsomos">
+			<h1 class="pb-2 mt-4 mb-2 border-bottom">Quem somos</h1>
+		</div>
+		
+		<div class="row about">
 			
-			<div class="row about">
-				
-				<div class="col-lg-6">
-					<img class="img-quemsomos esquerda-1 img-fluid" src="../img/nossaHistoria.jpg" alt="unidade franqueada">
-				</div>
-
-				<div class="col-lg-6">
-
-					<h3 class="quem-somos3">Missão</h3>
-
-					<p class="quem-somos">Oferecer ao mercado as melhores soluções em serviços de tecnologia, agregando valor de forma contínua às nossas atividades, tendo como base o comprometimento ético com nossos clientes e colaboradores</p>
-				</div>
-
-			</div>
-<hr>
-			<div class="row about">
-				
-				<div class="col-lg-6">
-					<h3>Visão</h3>
-
-					<p>Ser referência em qualidade de serviços de Informática do Brasil nos próximos anos, com o fundamental apoio dos nossos colaboradores e parceiros.</p>
-				</div>
-
-				<div class="col-lg-6">
-					<img class="img-quemsomos esquerda-1 img-fluid quem-somos" src="../img/nossaEstrutura.jpg" alt="unidade franqueada">
-					
-				</div>
-
-			</div>
-<hr>
-			<div class="row about">
-				
-				<div class="col-lg-6">
-					<img class="img-quemsomos esquerda-1 img-fluid" src="../img/compromisso.jpg" alt="unidade franqueada">
-				</div>
-
-				<div class="col-lg-6">
-
-					<h3 class="quem-somos3">Valores</h3>
-
-					<p class="quem-somos">Atitude Profissional – importar-se verdadeiramente com o Cliente e seus interesses, servindo-o, com entusiasmo, para atingir seus resultados. Atuação Ética e Responsável – atuar de forma ética, independente, responsável e com respeito à lei.</p>
-				</div>
-
-			</div>
-<hr>
-			<div class="row about">
-				
-				<div class="col-lg-6">
-					<h3>Objetivo</h3>
-
-					<p>Nosso objetivo é desenvolver e implementar soluções que buscam ajudar você a alcançar os objetivos do seu negócio, permitindo que você invista o seu tempo no que é mais importante para você, tornando a sua missão mais fácil de ser realizada e seus resultados muito melhores!</p>
-				</div>
-
-				<div class="col-lg-6">
-
-					<img class="img-quemsomos esquerda-1 img-fluid quem-somos" src="../img/nossaEstrutura.jpg" alt="unidade franqueada">
-					
-				</div>
-
+			<div class="col-lg-6">
+				<img class="img-quemsomos esquerda-1 img-fluid" src="../img/nossaHistoria.jpg" alt="unidade franqueada">
 			</div>
 
-<?php include 'Header.php'; ?>	
-	<div class="body-quemsomos">
-		<div class="container">
-			<div class="Titulo-quemsomos">
-				<h1>Quem Somos</h1>
+			<div class="col-lg-6">
+
+				<h3 class="quem-somos3">Missão</h3>
+
+				<p class="quem-somos">Oferecer ao mercado as melhores soluções em serviços de tecnologia, agregando valor de forma contínua às nossas atividades, tendo como base o comprometimento ético com nossos clientes e colaboradores</p>
 			</div>
-			<div class="row grid-quemsomos quemsomos-100px">
-				<div class="col-sm ">
-					<img class="img-quemsomos esquerda-1 img-fluid" src="../assets/img/nossaHistoria.jpg" alt="unidade franqueada">
-				</div>
-				<div class="col-sm texto-quemsomos direita ">
-					<h1>Missão</h1>
-					<p> Oferecer ao mercado as melhores soluções em serviços de tecnologia, agregando valor de forma contínua às nossas atividades,
-						tendo como base o comprometimento ético com nossos clientes e colaboradores </p>
-				</div>
-				</div>
-				<hr>
-
-			<div class="row grid-quemsomos quemsomos-100px">
-				<div class="col texto-quemsomos esquerda">
-					<h1>Visão</h1>
-					<p> Ser referência em qualidade de serviços de Informática do Brasil nos próximos anos, com o fundamental apoio dos nossos
-						colaboradores e parceiros. </p>
-				</div>
-				<div class="col-sm">
-					<img class="img-quemsomos direita img-fluid" src="../assets/img/nossaEstrutura.jpg" alt="uma grande estrutura para você">
-				</div>
-				</div>
-				<hr>
-			<div class="row grid-quemsomos quemsomos-100px">
-				<div class="col-sm ">
-					<img class="img-quemsomos esquerda-1 img-fluid" src="../assets/img/compromisso.jpg" alt="Nosso Compromisso é com você">
-				</div>
-				<div class="col texto-quemsomos direita">
-					<h1>Valores</h1>
-					<p> Atitude Profissional – importar-se verdadeiramente com o Cliente e seus interesses, servindo-o, com entusiasmo, para
-						atingir seus resultados. Atuação Ética e Responsável – atuar de forma ética, independente, responsável e com respeito
-						à lei.</p>
-				</div>
-				</div>
-				<hr>
-			<div class="row grid-quemsomos quemsomos-100px">
-				<div class="col texto-quemsomos esquerda">
-					<h1>Objetivo</h1>
-					<p> Nosso objetivo é desenvolver e implementar soluções que buscam ajudar você a alcançar os objetivos do seu negócio, permitindo
-						que você invista o seu tempo no que é mais importante para você, tornando a sua missão mais fácil de ser realizada
-						e seus resultados muito melhores! </p>
-				</div>
-				<div class="col-sm">
-					<img class="img-quemsomos direita img-fluid" src="../assets/img/nossaEstrutura.jpg" alt="uma grande estrutura para você">
-				</div>
-			</div>
-			<div class="row grid-quemsomos quemsomos-row">
-				<div class="col-sm order-sm-1">
-					<img class="img-quemsomos esquerda-1 img-fluid" src="../assets/img/nossaHistoria.jpg" alt="unidade franqueada">
-				</div>
-				<div class="col-sm texto-quemsomos direita order-sm-1 ">
-					<h1>Missão</h1>
-					<p> Oferecer ao mercado as melhores soluções em serviços de tecnologia, agregando valor de forma contínua às nossas atividades,
-						tendo como base o comprometimento ético com nossos clientes e colaboradores </p>
-				</div>
-				</div>
-			
-
-			<div class="row grid-quemsomos quemsomos-row">
-				<div class="col-sm">
-					<img class="img-quemsomos direita img-fluid" src="../assets/img/nossaEstrutura.jpg" alt="uma grande estrutura para você">
-				
-				<div class="col-sm texto-quemsomos esquerda">
-					<h1>Visão</h1>
-					<p> Ser referência em qualidade de serviços de Informática do Brasil nos próximos anos, com o fundamental apoio dos nossos
-						colaboradores e parceiros. </p>
-				</div>
-					</div>
-				 </div>
-
-			<div class="row grid-quemsomos quemsomos-row">
-				<div class="col-sm  order-sm-1">
-					<img class="img-quemsomos esquerda-1 img-fluid" src="../assets/img/compromisso.jpg" alt="Nosso Compromisso é com você">
-				</div>
-				<div class="col-sm texto-quemsomos direita order-sm-2">
-					<h1>Valores</h1>
-					<p> Atitude Profissional – importar-se verdadeiramente com o Cliente e seus interesses, servindo-o, com entusiasmo, para
-						atingir seus resultados. Atuação Ética e Responsável – atuar de forma ética, independente, responsável e com respeito
-						à lei.</p>
-				</div>
-				</div>
-			
-			<div class="row grid-quemsomos quemsomos-row">
-				<div class="col-sm ">
-					<img class="img-quemsomos direita img-fluid" src="../assets/img/nossaEstrutura.jpg" alt="uma grande estrutura para você">
-				</div>
-				<div class="col-sm texto-quemsomos esquerda">
-					<h1>Objetivo</h1>
-					<p> Nosso objetivo é desenvolver e implementar soluções que buscam ajudar você a alcançar os objetivos do seu negócio, permitindo
-						que você invista o seu tempo no que é mais importante para você, tornando a sua missão mais fácil de ser realizada
-						e seus resultados muito melhores! </p>
-					
-				</div>
-			</div>
-
 
 		</div>
+		<hr>
+		<div class="row about">
+			
+			<div class="col-lg-6">
+				<h3>Visão</h3>
+
+				<p>Ser referência em qualidade de serviços de Informática do Brasil nos próximos anos, com o fundamental apoio dos nossos colaboradores e parceiros.</p>
+			</div>
+
+			<div class="col-lg-6">
+				<img class="img-quemsomos esquerda-1 img-fluid quem-somos" src="../img/nossaEstrutura.jpg" alt="unidade franqueada">
+				
+			</div>
+
+		</div>
+		<hr>
+		<div class="row about">
+			
+			<div class="col-lg-6">
+				<img class="img-quemsomos esquerda-1 img-fluid" src="../img/compromisso.jpg" alt="unidade franqueada">
+			</div>
+
+			<div class="col-lg-6">
+
+				<h3 class="quem-somos3">Valores</h3>
+
+				<p class="quem-somos">Atitude Profissional – importar-se verdadeiramente com o Cliente e seus interesses, servindo-o, com entusiasmo, para atingir seus resultados. Atuação Ética e Responsável – atuar de forma ética, independente, responsável e com respeito à lei.</p>
+			</div>
+
+		</div>
+		<hr>
+		<div class="row about">
+			
+			<div class="col-lg-6">
+				<h3>Objetivo</h3>
+
+				<p>Nosso objetivo é desenvolver e implementar soluções que buscam ajudar você a alcançar os objetivos do seu negócio, permitindo que você invista o seu tempo no que é mais importante para você, tornando a sua missão mais fácil de ser realizada e seus resultados muito melhores!</p>
+			</div>
+
+			<div class="col-lg-6">
+
+				<img class="img-quemsomos esquerda-1 img-fluid quem-somos" src="../img/nossaEstrutura.jpg" alt="unidade franqueada">
+				
+			</div>
+
+		</div>
+
 	</div>
+</div>
 
 
 
 
 
 
-	<!-- INICIO FOOTER -->
-	<?php include 'footer.php'?>
+<!-- INICIO FOOTER -->
+<?php include 'footer.php'?>
