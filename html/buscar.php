4529b4e7739c8c99f57d84a8cc74353221aadd10
IgorWestermann <i.westma@gmail.com>
2018-11-24 07:41:10 -0200

    rows dos card apos pesquisar produtos em adamento

--- html/buscar.php
+++ html/buscar.php
@@ -1,4 +1,3 @@
-
 <?php include "header.php";?>
 <?php 
  $server = 'localhost';
@@ -17,9 +16,24 @@
 ?>
 
 
+<nav class="navbar navbar-expand-lg navbar-light categoria">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01"
+        aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse action-menu" id="navbarTogglerDemo01">
 
+        <ul class="list-group mr-auto mt-2 mt-lg-0  menu-list">
+            <?php include "nav-categoria.php"?>
+        </ul>
+    </div>
+</nav>
+<div class="container pag-produtos">
+    <h1>Resultado da pesquisa:
+        <?php echo $_POST['pesquisar']  ;?>
+    </h1><br>
 
-<?php
+    <?php
      if(isset($_POST['pesquisar'])){
          $pesquisar_p = $_POST['pesquisar'];
          $pesquisar_p = preg_replace("[#^0-9a-z#]","","$pesquisar_p");
@@ -37,22 +51,36 @@
                 // print_r("$prod");
                 // echo '<br>';
                 ?>
-                <div class="col-sm">
-      <div class="card img-card" style="width:16rem;">
-      <img class="card-img-top" src="../assets/imagem_banco/<?php echo $row['url_imagem']; ?>" alt="Card image cap">
-      <div class="card-body">
-      <h5 class="card-title"style='color:black; font-size:16px;'><?php echo $row['nome_produto'];?></h5>
-      <h5 style='color:red; font-size:26px;'>R$ <?php echo $row['preco'];?><h5>
-      </a>
-      </div>
-      </div>
-      </div>
-                <?php
-            }
-         }
-     }
 
-?>
+
+    <div class="row card-produto">
+
+        <a href="paginaProdutos.php?id_produto=<?php echo $row2['id_produto'];?>">
+
+            <div class="col-sm">
+                <div class="card img-card" style="width:16rem;">
+                    <img class="card-img-top" src="../assets/imagem_banco/<?php echo $row['url_imagem']; ?>" alt="Card image cap">
+                    
+                    <div class="card-body">
+                        <h5 class="card-title" style='color:black; font-size:16px;'>
+                            <?php echo $row['nome_produto'];?>
+                        </h5>
+                        <h5 style='color:red; font-size:26px;'>R$
+                            <?php echo $row['preco'];?>
+                        <h5>
+        </a>
+                    </div>
+                </div>
+            </div>
+    </div>
+<?php
+            }
+        }
+    }
+    
+    ?>
+</div>
+
 
 
 
