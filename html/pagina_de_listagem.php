03d138dbfb5da73fedf2fd48e53b6ab067c34802
unknown <josuedelgadoheringer98@gmail.com>
2018-12-07 01:49:16 -0200

    removendo autocomplete pesquisar

--- html/pagina_de_listagem.php
+++ html/pagina_de_listagem.php
@@ -45,7 +45,7 @@ if (isset($_SESSION['msg'])) {
   <nav class="navbar">
     <span style="font-size: 24px;"><a href="paginaprodutos-adm.php" class="fas fa-plus" alt="Cadastrar novo usuario"></a></span>
     <form class="form-inline my-2 my-lg-0" action="buscarLista.php?&pagina=1" enctype="multipart/form-data" method="GET">
-      <input class="form-control mr-sm-2" type="text" placeholder="Digite aqui..." name="pesquisar">
+      <input class="form-control mr-sm-2"  autocomplete="off" type="text" placeholder="Digite aqui..." name="pesquisar">
       <button class="btn btn-dark my-2 my-sm-0" type="submit">Pesquisar</button>
     </form>
   </nav>
